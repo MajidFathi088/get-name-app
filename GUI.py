7b333bf88b70fe364b7b62af1ef56787e69ecc49
majid <majidfathi088@gmail.com>
2026-01-07 10:08:06 +0100

    Implement the Add button

--- GUI.py
+++ GUI.py
@@ -1,10 +1,27 @@
 import functions
 import FreeSimpleGUI as sg
 
+
 lable = sg.Text("Enter your name: ")
-input_box = sg.InputText(tooltip="enter your name: ")
+input_box = sg.InputText(tooltip="enter your name: ", key="name")
 add_button = sg.Button("Add")
 
-window = sg.Window("The App", layout=[[[lable], [input_box, add_button]]])
-window.read()
+window = sg.Window("The App",
+                   layout=[[[lable], [input_box, add_button]]],
+                   font=('Helvetica', 12),)
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            names = functions.get_names()
+            new_name = values["name"] + "\n"
+            names.append(new_name)
+            functions.write_names(names)
+        case sg.WINDOW_CLOSED:
+            break
+
+
 window.close()
