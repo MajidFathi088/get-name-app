f385387fb438a205f95313548edb6d48eb0a6669
majid <majidfathi088@gmail.com>
2026-01-07 10:42:22 +0100

    Implement the Edit button

--- GUI.py
+++ GUI.py
@@ -3,11 +3,14 @@ import FreeSimpleGUI as sg
 
 
 lable = sg.Text("Enter your name: ")
-input_box = sg.InputText(tooltip="enter your name: ", key="name")
+input_box = sg.InputText(tooltip="enter your name: ", key="name", size=(20, 1))
+list_box = sg.Listbox(functions.get_names(), key="listbox", enable_events=True, size=(20, 10))
+edit_button = sg.Button("Edit")
 add_button = sg.Button("Add")
 
 window = sg.Window("The App",
-                   layout=[[[lable], [input_box, add_button]]],
+                   layout=[[[lable], [input_box, add_button],
+                            [list_box, edit_button]]],
                    font=('Helvetica', 12),)
 
 while True:
@@ -20,6 +23,20 @@ while True:
             new_name = values["name"] + "\n"
             names.append(new_name)
             functions.write_names(names)
+            window["listbox"].update(names)
+
+        case "Edit":
+            name_to_edit = values['listbox'][0]
+            new_name = values['name'] + '\n'
+
+            names = functions.get_names()
+            index = names.index(name_to_edit)
+            names[index] = new_name
+            functions.write_names(names)
+            window["listbox"].update(values=names)
+
+        case "listbox":
+            window["name"].update(value=values["listbox"][0])
         case sg.WINDOW_CLOSED:
             break
 
