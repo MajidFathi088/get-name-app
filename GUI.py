20da3291b082ace2383df34993def96aa0c744da
majid <majidfathi088@gmail.com>
2026-01-08 10:35:20 +0100

    Handle Errors, adding Clock, timeout, and change the theme

--- GUI.py
+++ GUI.py
@@ -1,7 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme('DarkGrey')
 
+clock = sg.Text("", key='clock')
 lable = sg.Text("Enter your name: ")
 input_box = sg.InputText(tooltip="enter your name: ", key="name", size=(20, 1))
 list_box = sg.Listbox(functions.get_names(), key="listbox", enable_events=True, size=(20, 10))
@@ -11,41 +14,54 @@ delete_button = sg.Button("Delete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("The App",
-                   layout=[[[lable],
+                   layout=[[[clock],
+                            [lable],
                             [input_box, add_button],
                             [list_box, edit_button, delete_button],
                             [exit_button]]],
                    font=('Helvetica', 12),)
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=500)
+    if event == sg.WIN_CLOSED:
+        break
+    window['clock'].update(value=time.strftime("%b %d, %y -%H:%M:%S"))
+
     match event:
         case "Add":
-            names = functions.get_names()
-            new_name = values["name"] + "\n"
-            names.append(new_name)
-            functions.write_names(names)
-            window["listbox"].update(names)
+            new_name = values["name"].strip()
+            if new_name:
+                names = functions.get_names()
+                names.append(new_name + "\n")
+                functions.write_names(names)
+                window["listbox"].update(values=names)
+                window["name"].update("")
+            else:
+                sg.popup("Name cannot be empty", font=('Helvetica', 12))
 
         case "Edit":
-            name_to_edit = values['listbox'][0]
-            new_name = values['name'] + '\n'
+            try:
+                name_to_edit = values['listbox'][0]
+                new_name = values['name'] + '\n'
 
-            names = functions.get_names()
-            index = names.index(name_to_edit)
-            names[index] = new_name
-            functions.write_names(names)
-            window["listbox"].update(values=names)
+                names = functions.get_names()
+                index = names.index(name_to_edit)
+                names[index] = new_name
+                functions.write_names(names)
+                window["listbox"].update(values=names)
+            except IndexError:
+                sg.popup("Please select a name.", font=('Helvetica', 12))
 
         case "Delete":
-            name_to_delete = values['listbox'][0]
-            names = functions.get_names()
-            names.remove(name_to_delete)
-            functions.write_names(names)
-            window["listbox"].update(values=names)
-            window["name"].update(value="")
+            try:
+                name_to_delete = values['listbox'][0]
+                names = functions.get_names()
+                names.remove(name_to_delete)
+                functions.write_names(names)
+                window["listbox"].update(values=names)
+                window["name"].update(value="")
+            except IndexError:
+                sg.popup("Please select a name.", font=('Helvetica', 12))
 
         case "Exit":
             break
